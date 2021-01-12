dd882ffa2db111a6591845b0067e93dee5d31595
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-01-12 16:40:40 -0500

    almost finished up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,93 @@
 // Your code goes here
+
+const contentPara = document.querySelector('.content-section .text-content p');
+const contentPara2 = contentPara.nextSibling.nextSibling;
+const inverseContentPara = document.querySelector('.inverse-content .text-content p');
+const inverseContentPara2 = inverseContentPara.nextSibling.nextSibling;
+const topNav = document.querySelector('.nav-container');
+const getH2 = document.querySelectorAll('h2');
+const getH2Array = Array.from(getH2);
+const buttons = document.querySelectorAll('.btn');
+const buttonsArray = Array.from(buttons);
+const homeButton = document.querySelector('.nav-link');
+const getBody = document.querySelector('body');
+const getLogo = document.querySelector('.logo-heading');
+
+
+// Hovering over the 'lets go' text changes the color of the text!
+
+contentPara.addEventListener('mouseover', (e) => {
+    contentPara.style.color = 'blue';
+});
+contentPara.addEventListener('mouseleave', (e) => {
+    contentPara.style.color = null;
+});
+
+contentPara2.addEventListener('mouseover', (e) => {
+    contentPara2.style.color = 'blue';
+});
+contentPara2.addEventListener('mouseleave', (e) => {
+    contentPara2.style.color = null;
+});
+
+// Hovering over the 'adventure awaits' text changes the color of the text!
+
+inverseContentPara.addEventListener('mouseover', (e) => {
+    inverseContentPara.style.color = 'red';
+});
+inverseContentPara.addEventListener('mouseleave', (e) => {
+    inverseContentPara.style.color = null;
+});
+inverseContentPara2.addEventListener('mouseover', (e) => {
+    inverseContentPara2.style.color = 'red';
+});
+inverseContentPara2.addEventListener('mouseleave', (e) => {
+    inverseContentPara2.style.color = null;
+});
+
+// If you're on the site and press 'F', an alert pops up telling you you're going to have a great time
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 70) {
+    alert(`You're about to have a great time!`);
+    }
+});
+
+//When scrolling, the nav bar stays at the top of the screen!
+
+topNav.addEventListener('scroll', (e) => {
+    topNav.style.position = 'fixed';
+    topNav.style.top = '0';
+    topNav.style.zIndex = '99';
+});
+
+// All the h2 tags change to orange when the page is resized!
+
+window.addEventListener('resize', (e) => {
+    getH2Array.forEach(element => {
+        element.style.color = 'orange';
+    });
+});
+
+// When the window loads, the hover animation on the buttons transition slower!
+
+window.addEventListener('load', (e) => {
+    buttonsArray.forEach(element => {
+        element.style.transition = '.3s';
+    });
+});
+
+// When the home button is double clicked, the body of the page turns gray!
+
+homeButton.addEventListener('dblclick', (e) => {
+    getBody.style.backgroundColor = 'lightGray';
+});
+
+// When the G key is unpressed, it toggles between green and black!
+
+window.addEventListener('keyup', (e) => {
+    if (e.keyCode === 71) {
+        getLogo.classList.toggle('green');
+    };
+});
+
